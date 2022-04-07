5c332e6f13274830a8be84ea974f5d24d6ac3929
lilin <leeliewhite@gmail.com>
2022-04-07 10:54:31 +0800

    disable rpc test

--- web3/src/test/scala/io/github/liewhite/web3/rpc/Deposit.scala
+++ web3/src/test/scala/io/github/liewhite/web3/rpc/Deposit.scala
@@ -1,28 +1,28 @@
-package io.github.liewhite.web3.rpc
+// package io.github.liewhite.web3.rpc
 
-import scala.language.postfixOps
-import org.scalatest.funsuite.AnyFunSuite
-import org.web3j.crypto.WalletUtils
-import org.web3j.crypto.Bip44WalletUtils
-import io.github.liewhite.web3.Extensions.*
-import io.github.liewhite.web3.wallet.Bip44Wallet
-import io.github.liewhite.web3.common
-import org.web3j.protocol.http.HttpService
-import org.web3j.protocol.Web3j
-import io.github.liewhite.web3.contract.ABIFunction
-import io.github.liewhite.web3.contract.types.ABIAddress
-import io.github.liewhite.web3.contract.types.ABIUintN
-import io.github.liewhite.web3.types.Address
+// import scala.language.postfixOps
+// import org.scalatest.funsuite.AnyFunSuite
+// import org.web3j.crypto.WalletUtils
+// import org.web3j.crypto.Bip44WalletUtils
+// import io.github.liewhite.web3.Extensions.*
+// import io.github.liewhite.web3.wallet.Bip44Wallet
+// import io.github.liewhite.web3.common
+// import org.web3j.protocol.http.HttpService
+// import org.web3j.protocol.Web3j
+// import io.github.liewhite.web3.contract.ABIFunction
+// import io.github.liewhite.web3.contract.types.ABIAddress
+// import io.github.liewhite.web3.contract.types.ABIUintN
+// import io.github.liewhite.web3.types.Address
 
-class DepositTest extends AnyFunSuite {
+// class DepositTest extends AnyFunSuite {
 
-  val depositFunc = ABIFunction[Unit,Unit]("deposit")
-  test("deposit"){
-    assert {
-      val deposit = "0xc778417E063141139Fce010982780140Aa0cD5Ab"
-      val receipt = Client.client.call(depositFunc)(Address.fromHex(deposit).!,(),value = 1 * common.Wei)
-      println(receipt)
-      receipt.isSuccess && receipt.get == ()
-    }
-  }
-}
+//   val depositFunc = ABIFunction[Unit,Unit]("deposit")
+//   test("deposit"){
+//     assert {
+//       val deposit = "0xc778417E063141139Fce010982780140Aa0cD5Ab"
+//       val receipt = Client.client.call(depositFunc)(Address.fromHex(deposit).!,(),value = 1 * common.Wei)
+//       println(receipt)
+//       receipt.isSuccess && receipt.get == ()
+//     }
+//   }
+// }
--- web3/src/test/scala/io/github/liewhite/web3/rpc/Erc20Symbol.scala
+++ web3/src/test/scala/io/github/liewhite/web3/rpc/Erc20Symbol.scala
@@ -1,35 +1,35 @@
-package io.github.liewhite.web3.rpc
+// package io.github.liewhite.web3.rpc
 
-import scala.language.postfixOps
-import org.scalatest.funsuite.AnyFunSuite
-import org.web3j.crypto.WalletUtils
-import org.web3j.crypto.Bip44WalletUtils
-import io.github.liewhite.web3.Extensions.*
-import io.github.liewhite.web3.wallet.Bip44Wallet
-import io.github.liewhite.web3.common
-import org.web3j.protocol.http.HttpService
-import org.web3j.protocol.Web3j
-import io.github.liewhite.web3.contract.ABIFunction
-import io.github.liewhite.web3.contract.types.ABIAddress
-import io.github.liewhite.web3.contract.types.ABIUintN
-import io.github.liewhite.web3.types.Address
-import io.github.liewhite.web3.contract.types.ABIString
+// import scala.language.postfixOps
+// import org.scalatest.funsuite.AnyFunSuite
+// import org.web3j.crypto.WalletUtils
+// import org.web3j.crypto.Bip44WalletUtils
+// import io.github.liewhite.web3.Extensions.*
+// import io.github.liewhite.web3.wallet.Bip44Wallet
+// import io.github.liewhite.web3.common
+// import org.web3j.protocol.http.HttpService
+// import org.web3j.protocol.Web3j
+// import io.github.liewhite.web3.contract.ABIFunction
+// import io.github.liewhite.web3.contract.types.ABIAddress
+// import io.github.liewhite.web3.contract.types.ABIUintN
+// import io.github.liewhite.web3.types.Address
+// import io.github.liewhite.web3.contract.types.ABIString
 
-class Erc20SymbolTest extends AnyFunSuite {
+// class Erc20SymbolTest extends AnyFunSuite {
 
-  val bFunc = ABIFunction[Unit, Tuple1[ABIString]]("symbol")
+//   val bFunc = ABIFunction[Unit, Tuple1[ABIString]]("symbol")
 
-  test("erc20 balance") {
-    assert {
-      val token = "0xc778417e063141139fce010982780140aa0cd5ab"
-      val receipt = Client.client.read(bFunc)(
-        Address.fromHex(token).!,
-        ()
-      )
-      if (receipt.isSuccess) {
-        println(receipt.get._1.value)
-      }
-      receipt.isSuccess
-    }
-  }
-}
+//   test("erc20 balance") {
+//     assert {
+//       val token = "0xc778417e063141139fce010982780140aa0cd5ab"
+//       val receipt = Client.client.read(bFunc)(
+//         Address.fromHex(token).!,
+//         ()
+//       )
+//       if (receipt.isSuccess) {
+//         println(receipt.get._1.value)
+//       }
+//       receipt.isSuccess
+//     }
+//   }
+// }
